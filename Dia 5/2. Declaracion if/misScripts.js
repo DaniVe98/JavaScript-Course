4f827f683ef8f747e78baffb8c06aa45ac9b990b
Daniel Vega <danielalbertovegabejarano@gmail.com>
2024-09-14 22:21:53 -0500

    Dia 5

---
+++ Dia 5/2. Declaracion if/misScripts.js
@@ -0,0 +1,10 @@
+function evaluarCompra(){
+  let elementoRespuesta = document.getElementById("decision");
+
+  let elementoPrecio = document.getElementById("textoPrecio");
+  let precio = elementoPrecio.value;
+
+  if(precio < 5){
+    elementoRespuesta.textContent = "Comprar 2 cartones de leche";
+  }
+}
