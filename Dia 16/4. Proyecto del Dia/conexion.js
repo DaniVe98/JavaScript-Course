6b6758c229362f16c24b7583564fc482d892acc8
Daniel Vega <danielalbertovegabejarano@gmail.com>
2024-09-14 23:38:06 -0500

    Update conexion.js

--- Dia 16/4. Proyecto del Dia/conexion.js
+++ Dia 16/4. Proyecto del Dia/conexion.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const conexion = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Inchcape@2023<',
+  password: '...',
   database: 'universidad'
 });
 
