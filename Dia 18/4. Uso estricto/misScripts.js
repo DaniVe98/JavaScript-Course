7feb2f02245106eaa3ea0f62dc7d9efd3495a50b
Daniel Vega <danielalbertovegabejarano@gmail.com>
2024-09-20 15:23:45 -0500

    4. Uso estricto

---
+++ Dia 18/4. Uso estricto/misScripts.js
@@ -0,0 +1,24 @@
+"use strict"
+
+// Omitting to define the varaibles------------------------------------------------------
+// x = 5;
+// console.log(x);
+
+
+// Deleting a variable with delete-------------------------------------------------------
+// let obj = {x: 5};
+// delete obj.x;
+// console.log(obj.x);
+
+
+// Duplicating a parameter---------------------------------------------------------------
+// function x(p1, p1){
+//   console.log(p1);
+// }
+
+// x("hola", "mundo");
+
+
+// Using reserved words in variables-----------------------------------------------------
+let public = "hola";
+console.log(public);
